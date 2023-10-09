90562b4f56119afe9d7b2fa22806643b8325489e
Alexis Martin <alexismartin1895@gmail.com>
2023-10-08 20:28:37 -0700

    moved meta tag

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,29 @@
 // @ts-nocheck
 import Hero from "./home/Hero";
+import Head from "next/head";
 
 export default function Home() {
 	return (
-		<div className="h-full overflow-hidden">
-			<Hero />
-		</div>
+		<>
+			<Head>
+				<link rel="shortcut icon" href="/static/favicon.ico" />
+				<title>Classmate</title>
+				<link
+					rel="apple-touch-icon"
+					href="/static/android-chrome-512x512.png"
+				/>
+				<meta
+					name="description"
+					content="Easily discover top professors and enhance your academic experience."
+					data-rh="true"
+				/>
+				<meta property="og:image" content="/static/cover.png" />
+				<meta name="twitter:image" content="/static/cover.png" />
+			</Head>
+
+			<div className="h-full overflow-hidden">
+				<Hero />
+			</div>
+		</>
 	);
 }
